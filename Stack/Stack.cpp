883d5766872d18b54c705728c6b4dc2017a2e9e1
cchoi <caleb.choi@uwaterloo.ca>
2019-10-16 19:12:28 -0400

    Added solution for 132 Pattern

---
+++ Stack/Stack.cpp
@@ -0,0 +1,42 @@
+#include "Stack.h"
+
+#include <stack>
+#include <vector>
+
+
+/*
+ * Medium
+ * 456. 132 Pattern
+ *
+ * O(n) stack + min array solution
+ * speed: 24 ms, faster than 83.92%
+ * memory: 10.9 MB, less than 25.00%
+ *
+ * https://leetcode.com/problems/132-pattern/
+ */
+bool find132pattern(const std::vector<int>& nums) {
+
+  if (nums.size() <= 2) return false;
+
+  std::vector<int> min;
+  min.reserve(nums.size());
+  min.push_back(nums[0]);
+
+  int m = nums[0];
+  for (int i = 1; i < nums.size(); ++i) {
+    if (nums[i] < m) m = nums[i];
+    min.push_back(m);
+  }
+
+  std::stack<int> s;
+
+  for (int i = nums.size() - 1; i >= 0; --i) {
+    while (!s.empty() && s.top() < nums[i]) {
+      if (s.top() > min[i]) return true;
+      else s.pop();
+    }
+    s.push(nums[i]);
+  }
+
+  return false;
+}
---
+++ Stack/Stack.h
@@ -0,0 +1,9 @@
+#ifndef CLION_STACK_H
+#define CLION_STACK_H
+
+#include <vector>
+
+// 456. 132 Pattern
+bool find132pattern(const std::vector<int>& nums);
+
+#endif //CLION_STACK_H
