20e62984184f838cf8657952c9a2aa0a5a82a953
cchoi <caleb.choi@uwaterloo.ca>
2019-10-16 22:09:04 -0400

    Added Daily Temperatures solution

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -3,7 +3,15 @@
 
 #include <vector>
 
+/*
+ * Medium
+ */
+
 // 456. 132 Pattern
 bool find132pattern(const std::vector<int>& nums);
+
+// 739. Daily Temperatures
+std::vector<int> dailyTemperatures(const std::vector<int>& T);
+
 
 #endif //CLION_STACK_H
