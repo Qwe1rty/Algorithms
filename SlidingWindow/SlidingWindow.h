a5b8936424172bbf61357244a83e559632d7848e
Qwe1rty <caleb.rhms@gmail.com>
2020-09-11 22:12:16 -0400

    Added solution for Sliding Window Substring

--- SlidingWindow/SlidingWindow.h
+++ SlidingWindow/SlidingWindow.h
@@ -1,6 +1,7 @@
 #ifndef CLION_SLIDINGWINDOW_H
 #define CLION_SLIDINGWINDOW_H
 
+#include <string>
 #include <vector>
 
 /*
@@ -16,6 +17,9 @@ int maxSubArray(const std::vector<int>& nums);
 
 // 239. Sliding Window Maximum
 std::vector<int> maxSlidingWindow(const std::vector<int>& nums, int k);
+
+// 76. Minimum Window Substring
+std::string minWindow(const std::string& s, const std::string& t);
 
 
 #endif //CLION_SLIDINGWINDOW_H
---
+++ SlidingWindow/Solutions/SlidingWindowSubstring.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <map>
+
+/*
+ * 76. Minimum Window Substring
+ * Hard
+ *
+ * speed: 52 ms, faster than 51.46%
+ * memory: 7.8 MB, less than 67.97%
+ *
+ * https://leetcode.com/problems/minimum-window-substring/
+ */
+std::string minWindow(const std::string& s, const std::string& t) {
+
+  bool valid{false};
+  int targets_reached{0}, l{0}, r{0}, best_l{0}, best_r(s.size());
+  std::map<char, int> targets{}, window_targets{};
+
+  for (const char& c : t) ++targets[c];
+
+  for (r = 0; r < s.size(); ++r) if (targets.count(s[r])) {
+
+    ++window_targets[s[r]];
+    if (window_targets[s[r]] == targets[s[r]]) ++targets_reached;
+
+    while (targets_reached == targets.size()) {
+      if (r - l < best_r - best_l) {
+        best_r = r;
+        best_l = l;
+      }
+
+      if (targets.count(s[l])) {
+        --window_targets[s[l]];
+        if (window_targets[s[l]] < targets[s[l]]) --targets_reached;
+      }
+
+      valid = true;
+      ++l;
+    }
+
+  }
+
+  return valid ? s.substr(best_l, best_r - best_l + 1) : "";
+}
